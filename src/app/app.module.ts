214ba5caa837e825587c7bbf657865f9b44d8510
Carlos Henrique <carlos.reis@graodireto.com.br>
2020-12-02 12:50:46 -0300

    Basic about services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     FlexLayoutModule,
   ],
-  providers: [],
+  providers: [
+    DishService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
