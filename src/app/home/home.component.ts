c5c41f11e7a2aff4694945cef584c539625715da
Carlos Henrique <carlos.reis@graodireto.com.br>
2020-12-04 13:36:09 -0300

    Assignement 2.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ɵConsole } from "@angular/core";
 
 import { Dish } from "../shared/dish.class";
 import { DishService } from "../services/dish.service";
 import { Promotion } from "../shared/promotion.class";
 import { PromotionService } from "../services/promotion.service";
+import { LeaderService } from "../services/leader.service";
+import { Leader } from "../shared/leader";
 
 @Component({
   selector: "app-home",
@@ -13,14 +15,17 @@ import { PromotionService } from "../services/promotion.service";
 export class HomeComponent implements OnInit {
   dish: Dish;
   promotion: Promotion;
+  leader: Leader;
 
   constructor(
     private dishService: DishService,
-    private promotionService: PromotionService
+    private promotionService: PromotionService,
+    private LeaderService: LeaderService
   ) {}
 
   ngOnInit() {
     this.dish = this.dishService.getFeaturedDish();
     this.promotion = this.promotionService.getFeaturedPromotion();
+    this.leader = this.LeaderService.getFeaturedLeader();
   }
 }
