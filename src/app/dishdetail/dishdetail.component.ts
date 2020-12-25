0047c0e07c7cd25e569318e181c561385f9bf734
Carlos Henrique <carlos.reis@graodireto.com.br>
2020-12-25 17:43:57 -0300

    Angular animation part two.

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,39 +5,18 @@ import { Dish } from "../shared/dish.class";
 import { switchMap } from "rxjs/operators";
 import { Comment } from "../shared/comment.class";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  animation,
-} from "@angular/animations";
 
 import { DishService } from "../services/dish.service";
+import { expand, flyInOut, visibility } from "../animations/app.animation";
 @Component({
   selector: "app-dishdetail",
   templateUrl: "./dishdetail.component.html",
   styleUrls: ["./dishdetail.component.scss"],
-  animations: [
-    trigger("visibility", [
-      state(
-        "shown",
-        style({
-          transform: "scale(1.0)",
-          opacity: 1,
-        })
-      ),
-      state(
-        "hidden",
-        style({
-          transform: "scale(0.5",
-          opacity: 0,
-        })
-      ),
-      transition("* => *", animate("0.5s ease-in-out")),
-    ]),
-  ],
+  host: {
+    "[@flyInOut]": "true",
+    style: "display: block;",
+  },
+  animations: [flyInOut(), visibility(), expand()],
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
